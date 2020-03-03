b330431e39873e810eed014807d9671c4dd3c4db
WallysonMarcos <wallyson78@gmail.com>
2020-03-03 19:44:59 -0400

    Subindo projeto

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+db = SQLAlchemy(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://suporte:$uportE99@localhost/atividades'
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+@app.route('/home')
+def home():
+    title = "Lista de Atividades"
+    lista = ['Atividade 1', 'Atividade 2']
+    return render_template('index.html', titulo=title, eventos=lista)
+
+
